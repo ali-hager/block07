05b7206e1827e2923a2651bb6f178ba584c4efcf
ali-hager <156130068+ali-hager@users.noreply.github.com>
2024-01-17 16:31:23 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// Step 1: Create three arithmetic operators, each generates a number (const), combo is 10-40-39 //
+const c1 = 8+2; //10//
+const c2 = 20*2; //40//
+const c3 = 41-2; //39//
+
+//Step 2: String message to the user: You have received this message because you have been chosen to open an important vault. Here is the secret combination://
+var message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+
+//Step 3: Displays combo in alert//
+alert(message + c1 + "-" + c2 + "-" + c3);
